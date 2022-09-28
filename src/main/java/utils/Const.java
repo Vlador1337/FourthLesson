d5a348faeab6b333a43665d160777ec5d709e63b
Vlador1337 <98329463+Vlador1337@users.noreply.github.com>
2022-09-29 00:10:41 +0500

    Delete src/main/java/utils directory

--- src/main/java/utils/Const.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public class Const {
-    public static final String JIRA_URL = "jiraUrl";
-    public static final String LOGIN = "login";
-    public static final String PASSWORD = "password";
-    public static final String TASK_NAME = "TestSelenium_bug";
-}
